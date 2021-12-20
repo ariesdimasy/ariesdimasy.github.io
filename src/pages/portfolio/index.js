92c58e4e453fea57279b061fd218206465474205
ariesdimasy <ariesdimasy@gmail.com>
2021-12-20 07:43:04 +0700

    update

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -5,15 +5,6 @@ export default function Portfolio() {
   return (
     <Content>
       <h2 className={`title2 `}> Portfolio </h2>
-      <Button
-        variant="primary"
-        onClick={() => {
-          alert("Hello");
-        }}
-      >
-        {" "}
-        Click Me{" "}
-      </Button>
     </Content>
   );
 }
